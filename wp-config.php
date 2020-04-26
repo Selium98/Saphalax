e8f8e7454b0ca3e45643adb233abdd2f41fd8825
Yash Kulkarni <60318826+Selium98@users.noreply.github.com>
2020-04-26 20:17:00 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -6,7 +6,7 @@
  * installation. You don't have to use the web site, you can
  * copy this file to "wp-config.php" and fill in the values.
  *
- * This file contains the following configurations:
+ * This contains the following configurations:
  *
  * * MySQL settings
  * * Secret keys
